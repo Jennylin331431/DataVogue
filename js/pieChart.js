0a170838a91214b87978b8be4a3e07b271f97936
farahmikati <farah.mikati@mail.utoronto.ca>
2025-03-24 01:35:40 -0400

    center pie chart

--- js/pieChart.js
+++ js/pieChart.js
@@ -22,8 +22,7 @@ class StackedPieChart{
             .attr("width", vis.pieWidth)
             .attr("height", vis.pieHeight)
             .append("g")
-            .attr("transform", "translate(" + vis.pieWidth / 2  + "," + vis.pieHeight / 2 + ")");
-
+            .attr("transform", `translate(${vis.pieWidth / 2 + 200}, ${vis.pieHeight / 2})`); 
         // Legend
         vis.legendContainer = d3.select("#brands-legend");
         vis.legendContainer.html(""); 
